df887c2fec67ace5c0261da960d01a6d2ee88ec1
Michael Fatemi <myfatemi04@gmail.com>
2022-04-16 17:32:45 -0400

    ModuleType

--- src/models/Module.ts
+++ src/models/Module.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 // id: string (“user/repo”)
 // title: string
 // markdown: string
@@ -7,19 +7,32 @@ import mongoose from 'mongoose'
 // thumbnail: string ← Parsed from md
 // authors: string ← Parsed from md
 
+export interface ModuleType {
+  id: string;
+  children: ModuleType[];
+  title: string;
+  markdown: string;
+}
+
 const ModuleSchema = new mongoose.Schema({
   id: {
     type: String,
-    required: true
+    required: true,
   },
-  childrenIds: [{
-    type: String
-  }],
+  title: {
+    type: String,
+    required: true,
+  },
+  childrenIds: [
+    {
+      type: String,
+    },
+  ],
   markdown: {
     type: String,
-    required: true
-  }
+    required: true,
+  },
 });
 
 const Module = mongoose.model("Module", ModuleSchema);
-export default Module
+export default Module;
